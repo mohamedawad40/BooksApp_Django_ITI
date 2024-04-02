5bee090b0379614b3691b1b05eed9b193d98880f
mohamed awad <mohamed.awad.elgammal@gmail.com>
2024-04-02 22:57:56 +0200

    connect to admiin dashboard

--- booksApp/books/models.py
+++ booksApp/books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    no_of_pages = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    price = models.IntegerField(null=True, blank=True)
+    image = models.CharField(max_length=200, null=True, blank=True)
+    # image = models.ImageField(upload_to='products/images', null=True, blank=True)
+    code = models.CharField(max_length=100 , unique=True, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
--- booksApp/booksApp/settings.py
+++ booksApp/booksApp/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'booksApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'booksapp_DB',
+        'USER':"os44",
+        "PASSWORD":"iti",
+        "PORT":3306,
+        "HOST":"localhost"
     }
 }
 
